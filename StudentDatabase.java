562097db1ca16417d3c444fb85f8eea9f813630f
avi20025 <100166555+avi20025@users.noreply.github.com>
2022-11-09 17:25:53 -0600

    Update StudentDatabase.java

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -1,11 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class StudentDatabase {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		//ask how many new users we want to add
-		 System.out.println("Enter number of new students to enroll: ");
+		 System.out.print("Enter number of new students to enroll: ");
 		 Scanner input = new Scanner(System.in);
 		 int size = input.nextInt();
 		 Student[] students = new Student[size];
@@ -16,7 +19,16 @@ public class StudentDatabase {
 		 	students[n] = new Student();
 		 	students[n].enroll();
 		 	students[n].payTuition();
-		 	System.out.println(students[n].toString());	
+		 	System.out.println("\n" + students[n].toString() + "\n");	
 		 }
+
+		 //printing result to a file to be stored in a database
+		 File myFile = new File("output.txt");
+		 myFile.createNewFile();
+		 PrintWriter myWriter = new PrintWriter(myFile);
+		 for (int i = 0; i < size; i++) {
+			myWriter.println(students[i].databaseRepresentation());
+		 }
+		 myWriter.close();
 	}
 }
